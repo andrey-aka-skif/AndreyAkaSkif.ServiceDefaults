c35b5f3dea06de8f8a4ed22a65bac65a928e4c59
andrey-aka-skif <2119163@gmail.com>
2025-12-04 03:50:09 +0300

    Create LoggingConfigureExtensions.cs

---
+++ src/AndeyAkaSkif.ServiceDefaults.Serilog/LoggingConfigureExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace AndeyAkaSkif.ServiceDefaults.Logging.Serilog;
+
+/// <summary>
+/// Предоставляет методы расширения для настройки логирования Serilog в приложении.
+/// </summary>
+public static class LoggingConfigureExtensions
+{
+    /// <summary>
+    /// Настраивает Serilog как систему логирования для приложения, 
+    /// используя конфигурацию из <see cref="IHostApplicationBuilder.Configuration"/>.
+    /// </summary>
+    /// <remarks>
+    /// В файле конфигурации приложения "appsettings.json" требуется секция "Serilog"
+    /// </remarks>
+    /// <param name="builder">
+    /// Экземпляр <see cref="IHostApplicationBuilder"/>, для которого настраивается логирование.
+    /// </param>
+    /// <returns>
+    /// Тот же экземпляр <see cref="IHostApplicationBuilder"/> для поддержки цепочки вызовов.
+    /// </returns>
+    public static IHostApplicationBuilder SetupSerilogLogging(this IHostApplicationBuilder builder)
+    {
+        var logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+
+        builder.Logging.AddSerilog(logger);
+
+        return builder;
+    }
+}
